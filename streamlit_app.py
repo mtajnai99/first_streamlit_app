9ef5bf95c7c12a8d233ac092571bb2899eb70a76
mtajnai99 <138032912+mtajnai99@users.noreply.github.com>
2023-06-29 08:01:14 -0500

    Update streamlit_app.py
    add avacado toast

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 import streamlit
 
-streamlit.title('My Parents new healthy Diner');
+streamlit.title('My Parents new healthy Diner');   
 streamlit.header('Breakfast Menu');
-streamlit.text('Omeaga 3 & Blueberry Oatmeal');
-streamlit.text('Kale, Spinach & Rocket Smoothie');
-streamlit.text('Hard Boiled Free-Range Egg');
+streamlit.text('🥣Omeaga 3 & Blueberry Oatmeal');
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie');
+streamlit.text('🐔Hard Boiled Free-Range Egg');
+streamlit.text('🥑🍞Avacado Toast');
